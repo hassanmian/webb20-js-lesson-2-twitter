4120669e0f8cd1b2968ecb2f8f9c27d416f38615
Hassan Mian <mian@willandskill.se>
2020-12-02 10:06:19 +0100

    Style Tweet

--- src/components/Tweet.jsx
+++ src/components/Tweet.jsx
@@ -4,17 +4,19 @@ import Content from './Content'
 import Date from './Date'
 import Thumbnail from './Thumbnail'
 
-export default function Tweet({author, date, content, imageURL}) {
-    return (
-        <div>
-            <div>
-                <Thumbnail imageURL={imageURL} />
-            </div>
-            <div>
-                <Author author={author} />
-                <Date date={date} />
-                <Content content={content} />
-            </div>
-        </div>
-    )
+export default function Tweet({ author, date, content, imageURL }) {
+  return (
+    <div className="row p-3 border mb-2">
+      <div className="col-sm-2">
+        <Thumbnail imageURL={imageURL} />
+      </div>
+      <div className="col-sm-6">
+        <Author author={author} />
+        <Content content={content} />
+      </div>
+      <div className="col-sm-4">
+        <Date date={date} />
+      </div>
+    </div>
+  )
 }
